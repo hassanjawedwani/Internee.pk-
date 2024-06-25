2ffa1cfcb78fd321cc9cea769c8b00846de6bc58
firstRepo <hassanjawedwani@gmail.com>
2024-06-26 04:08:12 +0500

    whyInterneecommplete

--- internee.pk/src/components/Home.js
+++ internee.pk/src/components/Home.js
@@ -148,6 +148,7 @@ const Home = () => {
         ]}
       />
       <WhyInternee />
+      div>
     </div>
   );
 };
--- internee.pk/src/components/WhyInternee.js
+++ internee.pk/src/components/WhyInternee.js
@@ -1,12 +1,70 @@
 import whyInterneeImage from '../assets/programing.jpg';
+import whyInterneeIcon from '../assets/Icon-flow-v2.svg';
 const WhyInternee = () => {
   return (
-    <div>
-      <div>
-        <h2>Why internee.pk?</h2>
+    <div style={{display: "flex", marginBottom: "200px"}}>
+      <div style={{width: "50%"}}>
+        <h2 style={{fontSize: "3rem"}}>Why internee.pk?</h2>
+
+        <div style={{display: "flex", marginTop: "27px",  }}>
+          <div style={{padding : "20px", border: "1px solid green", borderRadius: "10px" }}>
+            <img src={whyInterneeIcon} alt="why internee"/>
+          </div>
+          <div style={{display: "flex", alignItems: "center"}}> 
+            <h2 style={{fontSize: "1.4rem", ver: "center", paddingLeft: "30px" ,paddingRight: "120px"}}>5500+ Students already registered</h2>
+          </div>
+        </div>
+        <div style={{display: "flex", marginTop: "27px",  }}>
+          <div style={{padding : "20px", border: "1px solid green", borderRadius: "10px" }}>
+            <img src={whyInterneeIcon} alt="why internee"/>
+          </div>
+          <div style={{display: "flex", alignItems: "center"}}> 
+            <h2 style={{fontSize: "1.4rem", ver: "center", paddingLeft: "30px" ,paddingRight: "120px"}}>Task based projects that based on hands on experience</h2>
+          </div>
+        </div>
+        <div style={{display: "flex", marginTop: "27px",  }}>
+          <div style={{padding : "20px", border: "1px solid green", borderRadius: "10px" }}>
+            <img src={whyInterneeIcon} alt="why internee"/>
+          </div>
+          <div style={{display: "flex", alignItems: "center"}}> 
+            <h2 style={{fontSize: "1.4rem", ver: "center", paddingLeft: "30px" ,paddingRight: "120px"}}>Massive Courses with high-quality videos on LMS</h2>
+          </div>
+        </div>
+        <div style={{display: "flex", marginTop: "27px",  }}>
+          <div style={{padding : "20px", border: "1px solid green", borderRadius: "10px" }}>
+            <img src={whyInterneeIcon} alt="why internee"/>
+          </div>
+          <div style={{display: "flex", alignItems: "center"}}> 
+            <h2 style={{fontSize: "1.4rem", ver: "center", paddingLeft: "30px" ,paddingRight: "120px"}}>Empowering youth towards IT revolution & technology</h2>
+          </div>
+        </div>
+        <div style={{display: "flex", marginTop: "27px",  }}>
+          <div style={{padding : "20px", border: "1px solid green", borderRadius: "10px" }}>
+            <img src={whyInterneeIcon} alt="why internee"/>
+          </div>
+          <div style={{display: "flex", alignItems: "center"}}> 
+            <h2 style={{fontSize: "1.4rem", ver: "center", paddingLeft: "30px" ,paddingRight: "120px"}}>Hands on Experience via Virtual internship</h2>
+          </div>
+        </div>
+        <div style={{display: "flex", marginTop: "27px",  }}>
+          <div style={{padding : "20px", border: "1px solid green", borderRadius: "10px" }}>
+            <img src={whyInterneeIcon} alt="why internee"/>
+          </div>
+          <div style={{display: "flex", alignItems: "center"}}> 
+            <h2 style={{fontSize: "1.4rem", ver: "center", paddingLeft: "30px" ,paddingRight: "120px"}}>Complete your task and get market competent job</h2>
+          </div>
+        </div>
+        <div style={{display: "flex", marginTop: "27px",  }}>
+          <div style={{padding : "20px", border: "1px solid green", borderRadius: "10px" }}>
+            <img src={whyInterneeIcon} alt="why internee"/>
+          </div>
+          <div style={{display: "flex", alignItems: "center"}}> 
+            <h2 style={{fontSize: "1.4rem", ver: "center", paddingLeft: "30px" ,paddingRight: "120px"}}>Direct Collaboration with companies for direct hiring</h2>
+          </div>
+        </div>
       </div>
-      <div>
-        <img src={whyInterneeImage} alt="job apply" style={{width: "100%`"}}
+      <div style={{width: "50%", verticalAlign: "center", display: "flex", alignItems: "center"}}>
+        <img src={whyInterneeImage} alt="job apply" style={{width: "60%", height: "60%"}} />
       </div>
     </div>
   );
